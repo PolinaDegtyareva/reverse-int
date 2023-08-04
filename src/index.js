d45484a305c8c8a7840de1523c9b2fdcc64eee64
Polina <polina.degtyareva.99@gmail.com>
2023-08-04 15:30:04 +0300

    feat: add solusion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  //* проверить на отрицатльное число 
+  if (n < 0) { 
+    //* преобразовать из отрицательного в положительное 
+    n = Math.abs(n); 
+    //* преобразовать n в строку - 'n'
+    //* разбить строку на массив подстрок - ['n', 'n', 'n',...]
+    //* перевернуть массив
+    //* объединить элементы массива ['nnnn']
+    n = n.toString().split('').reverse().join(''); 
+    //* преобразовать строку в число - 'n' --> n
+    n = Number(n);
+
+    return n;
+  }
+
+  n = n.toString().split('').reverse().join('');
+  n = Number(n);
+
+  return n;
 }
